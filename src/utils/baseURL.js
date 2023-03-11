d26cca8434ab317b2761ba8c1dcf4dd90075da12
Emmanuel <emmanuel@Emmanuels-iMac.local>
2023-03-10 18:50:37 -0800

    new

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,2 @@
-// const baseURL = "http://localhost:2030/api/v1";
-const baseURL = "https://nodejs-ecommerce-api.onrender.com/api/v1";
+const baseURL = "http://localhost:2030/api/v1";
 export default baseURL;
